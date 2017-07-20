a2179686c9e7edd66ee453776f2cff37b0ed6e90
stephinwv <thekingdomseeker@aol.com>
2017-07-19 22:55:44 -0400

    Add files via upload
    Went in a whole new direction.

--- random_pairing.rb
+++ random_pairing.rb
@@ -1,23 +1,39 @@
 
+# def pair(names)
+#   pair = names.each_slice(2).to_a.shuffle
+  
+
+# end
+# def odd_ball(names)
+# 	if pair.length % 2 == 0
+# 		pair
+# 	else
+		
+# 		odd_ball = pair.pop
+# 		odd_ball << pair.new
+		
+# 	end
+# 	pair
+# end
 def pair(names)
-  pair = names
-  pair.shuffle
-  #This shuffles the arrays into random orders
-  pair.each_slice(2).to_a
-  #This puts two elements from the names and inserts them into an array
+	array = []
+	#Creates an empty array to hold objects later called
+	array_paired_names = names.shuffle.each_slice(2)
+	#Shuffles the names to create random picks, 
+	#takes the results and puts them in an array of (2)
+	#creating the pairs
 
-end
-def odd_ball(names)
-	if pair.length % 2 == 1
-		#if divisable by 2 with a remainder of 1
-		odd_ball = pair.pop
-
-		
-		pair.insert(odd_ball, 2)
-		
-	else
-		pair
+	array_paired_names.each do |pair|
+		#interates over each element in the array of names
+		if pair.length == 2
+			array << pair
+			#if the number of names is 2, print the pair
+		else
+			array.last << pair
+			#if there is a remainder, pushes the last array(of one)
+			#into the last returned pair
+		end
 	end
-	pair
+	array
+	
 end
-
